3786ba6a07e183d02f4c1f305100dfe39f1ab5c7
Vahan Aghajanyan <vahancho@gmail.com>
2020-06-28 14:25:28 +0200

    Add API documentation.

--- src/textable.h
+++ src/textable.h
@@ -28,22 +28,53 @@
 #include <iostream>
 #include <vector>
 
+//! Implements a textual table abstraction.
+/*!
+    A text table represents a table-like structure that can be streamed out as
+    a plain text. A table has cells that hold string values, each of which can be referred by
+    a row and column numbers.
+*/
 class Textable
 {
 public:
     using RowNumber    = size_t;
     using ColumnNumber = size_t;
 
+    //! Sets a value to the cell referred by the given \p row and \p column.
+    /*!
+        If table doesn't have the referred cell a new row and/or column will be added.
+        If such cell already exists, the \p value will be overwritten.
+        \param row    The row number
+        \param column The column number
+        \param value  A value. The type should be convertible to string.
+    */
     template<typename T>
     void setCell(RowNumber row, ColumnNumber column, T && value);
 
+    //! Sets a complete row values.
+    /*!
+        Allows to set up a complete row at once. The \p rowData is a container
+        of cell values.
+        \param row     The row number
+        \param rowData A container whose elements should be convertible to string.
+    */
     template<typename T>
     void setRow(RowNumber row, T && rowData);
 
+    //! Sets a complete column values.
+    /*!
+        Allows to set up a complete table column at once. The \p columnData is a container
+        of cell values.
+        \param column     The column number
+        \param columnData A container whose elements should be convertible to string.
+    */
     template<typename T>
     void setColumn(ColumnNumber column, T && columnData);
 
+    //! Returns the number of rows of the table.
     RowNumber rowCount() const;
+
+    //! Returns the number of columns of the table.
     ColumnNumber columnCount() const;
 
     friend std::ostream &operator<<(std::ostream &os, const Textable &table);
